8af25902d2bfcb764b9d66576819c45f2f384500
wolfper <pawel.haly@student.put.poznan.pl>
2015-02-09 23:40:20 +0000

    test

--- test2.py
+++ test2.py
@@ -8,3 +8,13 @@ def testtrue(money):
 testtrue(0)
 testtrue(20)
 testtrue(-1)
+
+def pasy(name = None, password = None):
+	while not (name and password):
+		if not name:
+			name = input('podaj imię:')
+		elif not password:
+			password = input('podaj haslo: ')
+	return name, password
+	
+pasy()
